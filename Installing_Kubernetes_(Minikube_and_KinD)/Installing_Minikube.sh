ad56985ebaefe28d67baa08ddf919e2dfdae98f7
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-06 23:53:00 +0100

    add

--- Installing_Kubernetes_(Minikube_and_KinD)/Installing_Minikube.sh
+++ Installing_Kubernetes_(Minikube_and_KinD)/Installing_Minikube.sh
@@ -1,4 +1,4 @@
 # Download the latest release of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
+# Install kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
