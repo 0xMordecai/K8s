5ef4fbc5daf763ae5c4b00c6da8a6ae8e33569f0
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-07 00:15:26 +0100

    add comment

--- Installing_Kubernetes_(Minikube_and_KinD)/Installing_Minikube.sh
+++ Installing_Kubernetes_(Minikube_and_KinD)/Installing_Minikube.sh
@@ -1,10 +1,11 @@
 # Download the latest release of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-# Install kubectl
+# Install kubectl to /usr/local/bin/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Test to ensure the version you installed is up-to-date
 kubectl version --client
+
 # download the minikube binary
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
-
+# Install kubectl to /usr/local/bin/minikube
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
