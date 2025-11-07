607fdf32825168a1c60f61ade6f1ed21081c21ec
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-07 22:32:58 +0100

    add comment

--- Installing_Kubernetes_(Minikube_and_KinD)/Installing_KinD.sh
+++ Installing_Kubernetes_(Minikube_and_KinD)/Installing_KinD.sh
@@ -4,3 +4,4 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
 chmod +x kind
 # move it to the default PATH directory
 sudo mv kind /usr/local/bin/
+# check whether KinD is installed
