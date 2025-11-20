e4e2c51d8713f08f2cd227de9d6cb3ddbb91540e
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-11-20 18:16:58 +0100

    add

--- Installing_Kubernetes_(Minikube_and_KinD)/install-kind/Installing_KinD.sh
+++
@@ -1,8 +0,0 @@
-# download KinD
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
-# make it executable
-chmod +x kind
-# move it to the default PATH directory
-sudo mv kind /usr/local/bin/
-# check whether KinD is installed
-kind version
